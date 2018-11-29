2909e9222d9c56643ab459ebf5d6f5c07ec6a7d5
m1n1027 <m1n1027@ezweb.ne.jp>
2018-11-29 20:00:30 +0900

    アニメーションとか色々バグってる
    途中

--- Assets/Scripts/Controller/PlayerAnimationController.cs
+++ Assets/Scripts/Controller/PlayerAnimationController.cs
@@ -2,16 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
+public enum PlayerAnimationNumber : int
+{
+    Idle      = 0,     //待機
+    Move      = 1,     //移動
+    Attack    = 2,     //攻撃
+    Avoidance = 3      //回避
+}
 public class PlayerAnimationController : MonoBehaviour {
 
-    punikon Punicon;
     int state = 0;
-
-    public readonly  int  IdleState = 0;        //待機
-    public readonly int MoveState = 1;        //移動
-    public readonly int AttackState = 2;      //攻撃
-    public readonly int AvoidanceState = 3;   //回避
 
     Animator anim;
     public float AnimNormalizeTime
@@ -22,7 +22,6 @@ public class PlayerAnimationController : MonoBehaviour {
         }
     }
 
-    public float foo() { return anim.GetCurrentAnimatorStateInfo(0).length; }
     public int State
     {
         get { return state; }
@@ -31,34 +30,20 @@ public class PlayerAnimationController : MonoBehaviour {
             anim.SetInteger("State", state);
         }
     }
+
+    public void ChangeAnimation(int animationNumber)
+    {
+        State = animationNumber;
+    }
+
+    public void ChangeAnimation(PlayerAnimationNumber animationNumber)
+    {
+        State = (int)animationNumber;
+    }
+
     private void Awake()
     {
-        Punicon = FindObjectOfType<punikon>();
         anim = GetComponent<Animator>();
     }
-    // Use this for initialization
-    IEnumerator Start () {
-
-        while(true)
-        {
-            //switch (Punicon.GetState())
-            //{
-            //    case 1:  //Move(Run)
-            //        state = MoveState;
-            //        anim.SetInteger("State", state);
-            //        break;
-            //    default:
-            //        state = 0;
-            //        anim.SetInteger("State", state);
-            //        break;
-            //}
-            yield return null;
-        }
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
-       
-	}
+    	
 }
--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -4,153 +4,130 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-public class PlayerController : MonoBehaviour {
-    private bool isAvoidance = false;
+public class PlayerController : MonoBehaviour
+{
+    private bool isPlaying = false;
     private punikon ogtInput;
     public Text dtext;
     /// <summary>
     /// NULL = Idle 
     /// </summary>
-    private Coroutine ActionState;
+    //private Coroutine ActionState;
     private PlayerAnimationController pAnim;
     Rigidbody rigidBody;
-    int state = 0;
+    private TouchState actionState = TouchState.NONE;
 
     BoxCollider boxCol;
     public bool isAttack = false;
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         ogtInput = FindObjectOfType<punikon>();
         pAnim = GetComponent<PlayerAnimationController>();
         rigidBody = GetComponent<Rigidbody>();
         boxCol = GetComponent<BoxCollider>();
         boxCol.enabled = false;
-  
-	}
+
+    }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A)) { ActionState = null; ActionState = StartCoroutine(Attack01()); }
-        //if (Input.touchCount < 1) return;
-        //int state = ogtInput.GetState();
-        var state = TouchParam.GetInstance.touchState;
-        switch (state)
+        actionState = TouchParam.GetInstance.touchState;
+        //if (pAnim.State == (int)PlayerAnimationNumber.Idle)
+        //{
+
+        //    rigidBody.velocity = Vector3.zero;
+        //    pAnim.ChangeAnimation(PlayerAnimationNumber.Idle);
+        //    isPlaying = false;
+        //}
+        StateTransition();
+        //if (TouchParam.GetInstance.touchState == TouchState.Move) pAnim.ChangeAnimation(PlayerAnimationNumber.Move);
+        switch (pAnim.State)
         {
-            // Idle
-            case TouchState.NONE:
-                //state = pAnim.IdleState;
+            case (int)PlayerAnimationNumber.Attack:
                 break;
-            // Attack
-            case TouchState.Tap:
-                if (state == TouchState.Move)
-                {
-                    StopCoroutine(ActionState);
-                    ActionState = null;
-                }
-                if (ActionState == null) ActionState = StartCoroutine(Attack01());
+            case (int)PlayerAnimationNumber.Avoidance:
+                Avoidance();
                 break;
-            // Move
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (pAnim.State == (int)PlayerAnimationNumber.Move)
+        {
+            Move();
+        }
+
+    }
+
+    void StateTransition()
+    {
+        PlayerAnimationNumber animNumber = PlayerAnimationNumber.Idle;
+        switch(TouchParam.GetInstance.touchState)
+        {
             case TouchState.Move:
-                if (ActionState == null) ActionState = StartCoroutine(Move());
+                animNumber = PlayerAnimationNumber.Move;
                 break;
-
-            // Avoidance
             case TouchState.Flick:
-                if (ActionState == null && (int)state != pAnim.AvoidanceState)
-                {
-                    isAvoidance = false;
-                    pAnim.State = pAnim.AvoidanceState;
-                }
-
+                animNumber = PlayerAnimationNumber.Avoidance;
+                break;
+            case TouchState.Tap:
+                animNumber = PlayerAnimationNumber.Attack;
                 break;
 
         }
+        pAnim.ChangeAnimation(animNumber);
+    }
 
-        switch(pAnim.State)
+    void Move()
+    {
+        if (!isPlaying) isPlaying = true;
+        var vec = TouchParam.GetInstance.touchDirection;
+        Vector3 dir = new Vector3(vec.x, 0, vec.y);
+        dir += transform.position;
+        transform.LookAt(dir);
+        transform.position += transform.forward * 0.1f;
+    }
+
+    void Attack01()
+    {
+        if (!isPlaying)
         {
-            case 3:
-                Avoidancee();
-                break;
+            boxCol.enabled = true;
+            isPlaying = true;
+        }
+
+        float value = pAnim.AnimNormalizeTime;
+        if (value >= 0.6f)
+        {
+            boxCol.enabled = false;
+            isPlaying = false;
+            pAnim.ChangeAnimation(PlayerAnimationNumber.Idle);
         }
     }
 
-    IEnumerator Move()
+    void Avoidance()
     {
-        state = pAnim.MoveState;
-        pAnim.State = pAnim.MoveState;
-        while (true)
+        if (!isPlaying)
         {
-            //int state = ogtInput.GetState();
-            var state = TouchParam.GetInstance.touchState;
-            if (state == TouchState.Move)
-            {
-                //var vec = ogtInput.GetDirection();
-                var vec = TouchParam.GetInstance.touchDirection;
-                Vector3 dir = new Vector3(vec.x, 0, vec.y);
-                dir += transform.position;
-                transform.LookAt(dir);
-                transform.position += transform.forward * 0.1f;
-
-            }
-            else break;
-
-            yield return true;
-        }
-
-        rigidBody.velocity = Vector3.zero;
-        pAnim.State = pAnim.IdleState;
-        ActionState = null;
-    }
-
-    IEnumerator Attack01()
-    {
-        state = pAnim.AttackState;
-        pAnim.State = pAnim.AttackState;
-        boxCol.enabled = true;
-        isAttack = true;
-        // TODO:攻撃した時の処理
-        float value = 0;
-        while (value < 0.7f)
-        {
-            value = pAnim.AnimNormalizeTime;
-            yield return null;
-        }
-
-        boxCol.enabled = false;
-        isAttack = false;
-
-        pAnim.State = pAnim.IdleState;
-        ActionState = null;
-        yield return null;
-    }
-
-    bool Avoidancee()
-    {
-        if (!isAvoidance)
-        {
-            pAnim.State = pAnim.AvoidanceState;
-            isAvoidance = true;
+            isPlaying = true;
             //フリックした方向にプレイヤーを回転
-            //var vec = ogtInput.GetDirection();
             var vec = TouchParam.GetInstance.touchDirection;
-            Vector3 dir = new Vector3(vec.x, 0, vec.y)*3;
+            Vector3 dir = new Vector3(vec.x, 0, vec.y) * 3;
             dir += transform.position;
             transform.LookAt(dir);
-            return false;
+            
         }
 
         var val = pAnim.AnimNormalizeTime;
         if (val >= 0.6f)        //終了
         {
-            isAvoidance = false;
-            pAnim.State =(TouchParam.GetInstance.touchState == TouchState.Move)?pAnim.MoveState:pAnim.IdleState;
-
-            return true;
+            isPlaying = false;
+            pAnim.ChangeAnimation(PlayerAnimationNumber.Idle);
         }
 
-
-        return false;
-
     }
-
+    
 }
+
